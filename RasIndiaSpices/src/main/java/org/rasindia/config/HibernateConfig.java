d7cbf00fe026cdc740cf8bd78d881303b785a484
Abhishek Arya <aarya526@gmail.com>
2018-07-28 02:27:11 +0530

    cofigurations

--- RasIndiaSpices/src/main/java/org/rasindia/config/HibernateConfig.java
+++ RasIndiaSpices/src/main/java/org/rasindia/config/HibernateConfig.java
@@ -1,5 +1,14 @@
 package org.rasindia.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({"org.rasindia.config"})
+@PropertySource(value = {"classpath: config.properties"})
 public class HibernateConfig {
 
 }
